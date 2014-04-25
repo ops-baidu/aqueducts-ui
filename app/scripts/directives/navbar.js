f45eb7ea1d85f4035578b89ab939acb4426f5939
liu zhuo <supersugarcane@126.com>
2014-04-25 11:00:41 +0800

    dynamic page view chart

--- app/scripts/directives/navbar.js
+++ app/scripts/directives/navbar.js
@@ -8,4 +8,20 @@ aqueductsApp.directive('navBar', function() {
     templateUrl: 'views/navbar.html'
   };
 });
+aqueductsApp.directive('hiChart', function() {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: { val: '=' },
+    link: function(scope, elm, attrs) {
+            scope.$watch('val', function(value) {
+            console.log(value);
+            value.chart.renderTo = elm[0] ;
+	    value.chart.width = 1100;
+            new Highcharts.Chart(value);
+           });
+    }
+  };
+});
+
 
