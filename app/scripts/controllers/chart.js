c3bb27dc5aeac8cc44d7e4e52f596cf0e832dfca
liu zhuo <supersugarcane@126.com>
2014-04-29 15:55:12 +0800

    dynamic chart auto get product,service name

--- app/scripts/controllers/chart.js
+++ app/scripts/controllers/chart.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var aqueductsApp = angular.module('webApp');
-aqueductsApp.controller('ChartController', function ($scope,$http,$q,Restangular) {
+aqueductsApp.controller('ChartController', function ($scope,$http,$q,$routeParams,Restangular) {
+  var product_id = $routeParams.product ; 
+  var service_id = $routeParams.service ; 
   $scope.initConfig=function(config,product,service,job) {
     var promises = [
 	Restangular.one('items', job.item_id).get(),
@@ -42,7 +44,7 @@ aqueductsApp.controller('ChartController', function ($scope,$http,$q,Restangular
       }
       $scope.config.title.text=item.name + " [" +calc.name + "]";
     });
-  }
+  };
 
   var credits =  {
     enabled: true,
@@ -52,104 +54,112 @@ aqueductsApp.controller('ChartController', function ($scope,$http,$q,Restangular
     }, 
     text: "Aqueducts"
   };
-  var promise = [ $http.get('http://api.aqueducts.baidu.com/v1/events?product=' + "im" + '&service=' + "router" + '&item=page_view&calculation=count&from=-16s&to=now')] ;
-  $q.all(promise).then(function (response) {
-    var result = []; 
-    var o_data = response[0].data ; 
-    for ( var i = 0; i < o_data.length ; i++ ) {
-      var value = o_data[i];
-      result.push({
-        x: value[0],
-        y: value[1] 
-      });
-    } 
-    Highcharts.setOptions({    
-       global: {    
-              useUTC: false    
-          }    
-    });  
-    $scope.data = {
-      chart: {
-          type: 'areaspline',
-          animation: Highcharts.svg, // don't animate in old IE
-          marginRight: 10,
-          events: {
-              load: function() {
-                  // set up the updating of the chart each second
-                  var series = this.series[0];
-                  setInterval(function() {
-                      var request = 'http://api.aqueducts.baidu.com/v1/events?product=' + "im" + '&service=' + "router" + '&item=page_view&calculation=count&from=-16s&to=now' ;
-                      $http.get(request).success(function (data, status, headers, config) {
-                        if ( data[data.length - 1] != series[series.length - 1]) {
-                          series.addPoint( [data[data.length -1][0], data[data.length -1][1] ], true, true) ; 
-                        }
-                      });
-                  }, 1000);
-              }
-          }
-      },
-      title: {
-          text: 'Realtime Page View'
-      },
-      xAxis: {
-          type: 'datetime',
-          tickPixelInterval: 75
-      },
-      yAxis: {
-          min: 0,
-          title: {
-              text: 'query / s'
+  var promises = [
+      Restangular.one('products', product_id).get(),
+      Restangular.one('products', product_id).one('services', service_id).get()
+  ];
+  $q.all(promises).then(function(results) {
+    var product = results[0] ;
+    var service = results[1] ;
+    var promise = [ $http.get('http://api.aqueducts.baidu.com/v1/events?product=' + product.name + '&service=' + service.name + '&item=page_view&calculation=count&from=-16s&to=now')] ;
+    $q.all(promise).then(function (response) {
+      var result = []; 
+      var o_data = response[0].data ; 
+      for ( var i = 0; i < o_data.length ; i++ ) {
+        var value = o_data[i];
+        result.push({
+          x: value[0],
+          y: value[1] 
+        });
+      } 
+      Highcharts.setOptions({    
+         global: {    
+                useUTC: false    
+            }    
+      });  
+      $scope.data = {
+        chart: {
+            type: 'areaspline',
+            animation: Highcharts.svg, // don't animate in old IE
+            marginRight: 10,
+            events: {
+                load: function() {
+                    // set up the updating of the chart each second
+                    var series = this.series[0];
+                    setInterval(function() {
+                        var request = 'http://api.aqueducts.baidu.com/v1/events?product=' + product.name + '&service=' + service.name + '&item=page_view&calculation=count&from=-16s&to=now' ;
+                        $http.get(request).success(function (data, status, headers, config) {
+                          if ( data[data.length - 1] != series[series.length - 1]) {
+                            series.addPoint( [data[data.length -1][0], data[data.length -1][1] ], true, true) ; 
+                          }
+                        });
+                    }, 1000);
+                }
+            }
+        },
+        title: {
+            text: 'Realtime Page View'
+        },
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 75
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'query / s'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        credits: credits,
+        plotOptions: {
+          areaspline: {
+            fillColor: {
+              linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+              stops: [
+                [0, Highcharts.getOptions().colors[0]],
+                [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')],    
+              ],
+            },
           },
-          plotLines: [{
-              value: 0,
-              width: 1,
-              color: '#808080'
-          }]
-      },
-      credits: credits,
-      plotOptions: {
-        areaspline: {
-          fillColor: {
-            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-            stops: [
-              [0, Highcharts.getOptions().colors[0]],
-              [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')],    
-            ],
-          },
-        },
-        lineWidth: 1,
-        marker: {
-          enabled: true,
-          radius: 1
-        },
-        shadow: false,
-        states: {
-          hover: {
-            lineWidth: 1,
-          },
-        },
-        threshold: null,
-      },
-     
-      tooltip: {
-        crosshairs: {
-          enable: true,
-          width: 2,
-        },
-      },
-      legend: {
-          enabled: false
-      },
-      exporting: {
-          enabled: false
-      },
-      series: [{
-          name: 'query',
+          lineWidth: 1,
           marker: {
-            enabled: false,
+            enabled: true,
+            radius: 1
           },
-          data: result,
-      }]
-    }
+          shadow: false,
+          states: {
+            hover: {
+              lineWidth: 1,
+            },
+          },
+          threshold: null,
+        },
+       
+        tooltip: {
+          crosshairs: {
+            enable: true,
+            width: 2,
+          },
+        },
+        legend: {
+            enabled: false
+        },
+        exporting: {
+            enabled: false
+        },
+        series: [{
+            name: 'query',
+            marker: {
+              enabled: false,
+            },
+            data: result,
+        }]
+      }
+    });
   });
 });
--- app/scripts/directives/navbar.js
+++ app/scripts/directives/navbar.js
@@ -12,14 +12,17 @@ aqueductsApp.directive('hiChart', function() {
   return {
     restrict: 'E',
     replace: true,
-    scope: { val: '=' },
+    scope: { 
+       val: '=' ,
+       product: '@',
+       service: '@',
+    },
     link: function(scope, elm, attrs) {
-            scope.$watch('val', function(value) {
-            console.log(value);
-            value.chart.renderTo = elm[0] ;
-	    value.chart.width = 1100;
-            new Highcharts.Chart(value);
-           });
+      scope.$watch('val', function(value) {
+        value.chart.renderTo = elm[0] ;
+        value.chart.width = 1100;
+        new Highcharts.Chart(value);
+      });
     }
   };
 });
