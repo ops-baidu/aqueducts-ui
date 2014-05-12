30558245cd0661340c51d165cada020cdc389af2
liu zhuo <supersugarcane@126.com>
2014-05-12 22:22:47 +0800

    job-modalInstance use right inject way

--- app/scripts/controllers/job.js
+++ app/scripts/controllers/job.js
@@ -54,110 +54,105 @@ aqueductsApp.controller('JobController', ['$modal', '$route','$log','$scope', '$
         $scope.calculation = calculation;  
       });
       job.all('tags').getList().then(function(tags) {
-	var tag_names = [];
-	for(var i=0; i< tags.length; i++) {
-	  tag_names.push(tags[i].name);
-	}
+      	var tag_names = [];
+      	for(var i=0; i< tags.length; i++) {
+      	  tag_names.push(tags[i].name);
+      	}
         $scope.tag_names = tag_names;  
       });
     };
     // create job
     $scope.new = function() {
 
-      $scope.job = {
-        name: $scope.product.name + "_" + $scope.service.name,
-	product_id: '',
-	service_id: '',
-        item_id: '',
-        calculation_id: '',
-        tag_ids: ''	
-      };
       var modalInstance = $modal.open({
         templateUrl: 'views/job_detail.html' ,
-	controller: ModalInstanceCtrl,
-	resolve: {
-	  product: function() {
-	    return $scope.product ;  
-	  },
-	  service: function() {
-	    return $scope.service ;  
-	  },
-	  job: function() {
-	    return $scope.job;  
-	  },
-	  items: function() {
-	    return $scope.items;  
-	  },
-	  tags: function() {
-	    return $scope.tags;  
-	  }
-	}
+	      controller: ModalInstanceCtrl,
       });
 
       modalInstance.result.then(function(scope) {
-	var product_id = scope.product.id ; 
-	var service_id = scope.service.id ; 
-	var job = scope.job ; 
+      	var product_id = scope.product.id ; 
+      	var service_id = scope.service.id ; 
+      	var job = scope.job ; 
         var ids = scope.ids ; 
 
-	var ids_index = [];
-	for ( var id in ids ) {
-          if ( ids[id] == true ) {
-	    ids_index.push(id) ;
-	  }	
-	}
+      	var ids_index = [];
+      	for ( var id in ids ) {
+                if ( ids[id] == true ) {
+      	    ids_index.push(id) ;
+      	  }	
+      	}
 
-	console.log(ids_index);
-
-	job.calculation_id = job.calc.id ; 
-	job.item_id = job.item.id ; 
-	job.product_id = product_id;
-	job.service_id = service_id;
-	job.tag_ids = ids_index ; 
-	job.name = scope.product.name 
-	         + "_" + scope.service.name 
-	         + "_" + job.item.name 
-	         + "_" + job.calc.name ; 
-	console.log(job);
+	     job.calculation_id = job.calc.id ; 
+	     job.item_id = job.item.id ; 
+	     job.product_id = product_id;
+	     job.service_id = service_id;
+	     job.tag_ids = ids_index ; 
+	     job.name = scope.product.name 
+	              + "_" + scope.service.name 
+	              + "_" + job.item.name 
+	              + "_" + job.calc.name ; 
+	     console.log(job);
 
 
-	var jobs = Restangular.one('products', product_id).one('services', service_id).all('jobs') ; 
-        jobs.post(job).then(function() {
-          $route.reload(); 
-        });
-      });
-   };
-
-   var ModalInstanceCtrl = function($scope,$modalInstance,product,service,job,items,tags) {
-     $scope.product = product ; 
-     $scope.service = service ;  
-     $scope.job = job;  
-     $scope.items = items ; 
-     $scope.tags = tags ; 
-
-     var ids = {};
-     for ( var i=0; i<tags.length ; i++ ) {
-       var id = tags[i].id ; 
-       ids[id] = false ;
-     }
-     $scope.ids = ids ; 
-     $scope.ok = function() {
-       $modalInstance.close($scope);
-     };
-
-     $scope.change_calc = function() {
-       $scope.job.calc = null;  
-       Restangular.one('items', $scope.job.item.id).all('calculations').getList().then(function(calcs) {
-         $scope.calcs = calcs;
+	     var jobs = Restangular.one('products', product_id).one('services', service_id).all('jobs') ; 
+       jobs.post(job).then(function() {
+         $route.reload(); 
        });
-     };
-
-     $scope.require = function() {
-	if ( $scope.job.item && $scope.job.calc ) return false;
-	return true ; 
-     };
-     $scope.cancel = function() {
-       $modalInstance.dismiss('cancel');	
-     };
+     });
    };
+
+   var ModalInstanceCtrl = function($scope,$modalInstance,$routeParams,Restangular,$q) {
+     var product_id = $routeParams.product ; 
+     var service_id = $routeParams.service ; 
+     var promises = [
+       Restangular.one('products', product_id).get(),
+       Restangular.one('products', product_id).one('services', service_id).get(),
+       Restangular.all('items').getList(),
+       Restangular.all('tags').getList()
+     ];
+     $q.all(promises).then(function(results) {
+       var product = results[0] ;
+       var service = results[1] ;
+       var items = results[2];
+       var tags = results[3];
+       $scope.product = product ; 
+       $scope.service = service ; 
+       $scope.items = items ; 
+       $scope.tags = tags ; 
+       $scope.job = {
+         name: product.name + "_" + service.name,
+         product_id: '',
+         service_id: '',
+         item_id: '',
+         calculation_id: '',
+         tag_ids: ''	
+       };
+
+       var ids = {};
+       for ( var i=0; i<tags.length ; i++ ) {
+         var id = tags[i].id ; 
+         ids[id] = false ;
+       }
+       $scope.ids = ids ; 
+       $scope.ok = function() {
+         $modalInstance.close($scope);
+       };
+
+       $scope.change_calc = function() {
+         $scope.job.calc = null;  
+         Restangular.one('items', $scope.job.item.id).all('calculations').getList().then(function(calcs) {
+           $scope.calcs = calcs;
+         });
+       };
+
+       $scope.require = function() {
+         if ( $scope.job.item && $scope.job.calc ) return false;
+         return true ; 
+       };
+       $scope.cancel = function() {
+         $modalInstance.dismiss('cancel');	
+       };
+       });
+   };
+   ModalInstanceCtrl.$inject = ['$scope','$modalInstance','$routeParams','Restangular','$q'];
 }]);
