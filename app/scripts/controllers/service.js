09e2cc831c629da7c7a270db70c9c4c8822ec518
liu zhuo <supersugarcane@126.com>
2014-05-13 13:42:19 +0800

    delete code : if newService

--- app/scripts/controllers/service.js
+++ app/scripts/controllers/service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var aqueductsApp = angular.module('webApp');
-aqueductsApp.controller('ServiceController', ['$modal', '$route','$log','$scope', '$routeParams', '$location','Restangular', function($modal, $route, $log, $scope, $routeParams, $location, Restangular, service) {
+aqueductsApp.controller('ServiceController', ['$modal', '$route','$log','$scope', '$routeParams', '$location','Restangular', function($modal, $route, $log, $scope, $routeParams, $location, Restangular) {
     var product_id = $routeParams.product ; 
     var services = Restangular.one('products', product_id).all('services');
     var product = Restangular.one('products', product_id);
@@ -15,7 +15,6 @@ aqueductsApp.controller('ServiceController', ['$modal', '$route','$log','$scope'
     // for job number
     $scope.initService = function(product, service) {
       Restangular.one('products', product.id).one('services', service.id).all('jobs').getList().then(function(jobs) {
-	console.log(jobs.length);
         $scope.job_number = jobs.length ;  
       });
     };
@@ -23,7 +22,6 @@ aqueductsApp.controller('ServiceController', ['$modal', '$route','$log','$scope'
     // destroy service
     $scope.destroy = function(service) {
       service.remove().then(function() {
-        console.log($route);
         $route.reload();
       });
     };
@@ -36,24 +34,13 @@ aqueductsApp.controller('ServiceController', ['$modal', '$route','$log','$scope'
     $scope.edit = function (product, service) {
 
       var services = Restangular.one('products', product.id).all('services');
-
       $scope.base_services = services ; 
-      if (service == 'new') {
-         $scope.newService = true;
-         $scope.service = { name: ''};
-      }
-      else {
-        $scope.newService = false;
-        $scope.service = service;
-      } 
+      $scope.service = { name: ''};
    
       var modalInstance = $modal.open({
         templateUrl: 'views/service_detail.html',
         controller: ModalInstanceCtrl,
         resolve: {
-          newService: function () {
-            return $scope.newService;
-          },
           services: function () {
             return $scope.base_services;
           },
@@ -66,35 +53,25 @@ aqueductsApp.controller('ServiceController', ['$modal', '$route','$log','$scope'
       modalInstance.result.then(function (scope) {
         var name = scope.service.name ; 
         var services = scope.services ; 
-        if ( scope.newService ) {
-          service.name = name ; 
-          var a_service = {
-            name: name,
-          };
-          services.post(a_service).then(function() {
-            $route.reload();
-          });
-        } else {
-          service.get().then(function(service) {
-            service.name = name ; 
-            service.put().then(function() {
-              $route.reload();
-            }) ; 
-          });
-        }
+        service.name = name ; 
+        var a_service = {
+          name: name,
+        };
+        services.post(a_service).then(function() {
+          $route.reload();
+        });
       }, function () {
         $log.info('Modal dismissed at: ' + new Date());
       });
     };
 
-    var ModalInstanceCtrl = function ($scope, $modalInstance, newService, service, services) {
-      $scope.newService = newService;
+    var ModalInstanceCtrl = function ($scope, $modalInstance, service, services) {
       $scope.service = service ; 
       $scope.services = services ; 
      
       $scope.require = function() {
         if ( $scope.service.name ) return false ; 
-	return true ; 
+	      return true ; 
       };
 
       $scope.ok = function () {
