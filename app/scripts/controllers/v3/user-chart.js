04812b8345364db8538cc1bdd887833a042302c4
starbit <129.911@gmail.com>
2014-08-27 20:10:35 +0800

    still have problems with high chart drawing

--- app/scripts/controllers/v3/user-chart.js
+++ app/scripts/controllers/v3/user-chart.js
@@ -85,113 +85,106 @@ aqueductsApp.controller('UserChartController', [ '$scope','$http', '$q', '$route
       },
       text: "Aqueducts"
     };
-    var promises = [
-        Restangular.all('user').customGET('info').get(),
-        Restangular.all('user').one('services', service_name).get()
-    ];
-    $q.all(promises).then(function(results) {
-      var user = results[0] ;
-      var service = results[1] ;
-      var promise = [ $http.get(EventsApiBaseUrl + 'events?product=' + user.name + '&service=' + service.name + '&item=page_view&calculation=count&from=-16s&to=now')] ;
-      $q.all(promise).then(function (response) {
-        var result = [];
-        var o_data = response[0].data ;
-        for ( var i = 0; i < o_data.length ; i++ ) {
-          var value = o_data[i];
-          result.push({
-            x: value[0],
-            y: value[1]
-          });
-        }
-        Highcharts.setOptions({
-          global: {
-            useUTC: false
-          }
-        });
-        $scope.data = {
-          chart: {
-            type: 'areaspline',
-            animation: Highcharts.svg, // don't animate in old IE
-            marginRight: 10,
-            events: {
-              load: function() {
-                // set up the updating of the chart each second
-                var series = this.series[0];
-                setInterval(function() {
-                  var request = EventsApiBaseUrl + 'events?product=' + user.name + '&service=' + service.name + '&item=page_view&calculation=count&from=-16s&to=now' ;
-                  $http.get(request).success(function (data, status, headers, config) {
-                    if ( data[data.length - 1] != series[series.length - 1]) {
-                      series.addPoint( [data[data.length -1][0], data[data.length -1][1] ], true, true) ;
-                    }
-                  });
-                }, 1000);
-              }
-            }
-          },
-          title: {
-            text: 'Realtime Page View'
-          },
-          xAxis: {
-            type: 'datetime',
-            tickPixelInterval: 75
-          },
-          yAxis: {
-            min: 0,
-            title: {
-                text: 'query / s'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-          },
-          credits: credits,
-          plotOptions: {
-            areaspline: {
-              fillColor: {
-                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-                stops: [
-                  [0, Highcharts.getOptions().colors[0]],
-                  [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')],
-                ],
-              },
-            },
-            lineWidth: 1,
-            marker: {
-              enabled: true,
-              radius: 1
-            },
-            shadow: false,
-            states: {
-              hover: {
-                lineWidth: 1,
-              },
-            },
-            threshold: null,
-          },
 
-          tooltip: {
-            crosshairs: {
-              enable: true,
-              width: 2,
-            },
-          },
-          legend: {
-              enabled: false
-          },
-          exporting: {
-              enabled: false
-          },
-          series: [{
-              name: 'query',
-              marker: {
-                enabled: false,
-              },
-              data: result,
-          }]
+    var promise = [ $http.get(EventsApiBaseUrl + 'events?product=' + username + '&service=' + service_name + '&item=page_view&calculation=count&from=-16s&to=now')] ;
+    $q.all(promise).then(function (response) {
+      var result = [];
+      var o_data = response[0].data ;
+      for ( var i = 0; i < o_data.length ; i++ ) {
+        var value = o_data[i];
+        result.push({
+          x: value[0],
+          y: value[1]
+        });
+      }
+      Highcharts.setOptions({
+        global: {
+          useUTC: false
         }
       });
+      $scope.data = {
+        chart: {
+          type: 'areaspline',
+          animation: Highcharts.svg, // don't animate in old IE
+          marginRight: 10,
+          events: {
+            load: function() {
+              // set up the updating of the chart each second
+              var series = this.series[0];
+              setInterval(function() {
+                var request = EventsApiBaseUrl + 'events?product=' + username + '&service=' + service_name + '&item=page_view&calculation=count&from=-16s&to=now' ;
+                $http.get(request).success(function (data, status, headers, config) {
+                  if ( data[data.length - 1] != series[series.length - 1]) {
+                    series.addPoint( [data[data.length -1][0], data[data.length -1][1] ], true, true) ;
+                  }
+                });
+              }, 1000);
+            }
+          }
+        },
+        title: {
+          text: 'Realtime Page View'
+        },
+        xAxis: {
+          type: 'datetime',
+          tickPixelInterval: 75
+        },
+        yAxis: {
+          min: 0,
+          title: {
+              text: 'query / s'
+          },
+          plotLines: [{
+              value: 0,
+              width: 1,
+              color: '#808080'
+          }]
+        },
+        credits: credits,
+        plotOptions: {
+          areaspline: {
+            fillColor: {
+              linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+              stops: [
+                [0, Highcharts.getOptions().colors[0]],
+                [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')],
+              ],
+            },
+          },
+          lineWidth: 1,
+          marker: {
+            enabled: true,
+            radius: 1
+          },
+          shadow: false,
+          states: {
+            hover: {
+              lineWidth: 1,
+            },
+          },
+          threshold: null,
+        },
+
+        tooltip: {
+          crosshairs: {
+            enable: true,
+            width: 2,
+          },
+        },
+        legend: {
+            enabled: false
+        },
+        exporting: {
+            enabled: false
+        },
+        series: [{
+            name: 'query',
+            marker: {
+              enabled: false,
+            },
+            data: result,
+        }]
+      }
     });
   };
 
