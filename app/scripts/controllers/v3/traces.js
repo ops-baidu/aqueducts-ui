e421db7e8eea4e721fd2e5829f77573f7f73eed4
zhaoxuan <zhaoxuan1727@gmail.com>
2014-10-13 14:21:59 +0800

    add modal to show trace detail

--- app/scripts/controllers/v3/traces.js
+++ app/scripts/controllers/v3/traces.js
@@ -2,12 +2,12 @@
 * @Author: john
 * @Date:   2014-09-28 11:37:44
 * @Last Modified by:   john
-* @Last Modified time: 2014-10-10 13:54:14
+* @Last Modified time: 2014-10-13 14:21:02
 */
 
 'use strict';
 
-angular.module('webApp').controller('TracesController', ['$scope', '$http', 'ApiBaseUrl', '$routeParams', function($scope, $http, ApiBaseUrl, $routeParams) {
+angular.module('webApp').controller('TracesController', ['$scope', '$http', 'ApiBaseUrl', '$routeParams', '$modal', function($scope, $http, ApiBaseUrl, $routeParams, $modal) {
 
   var traceId = $routeParams.traceId;
   var serviceName = $routeParams.serviceName;
@@ -27,6 +27,37 @@ angular.module('webApp').controller('TracesController', ['$scope', '$http', 'Api
   };
 
   getTrace();
+
+  $scope.showModal = function(span) {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'views/v3/trace_detail.html',
+      controller: ModalInstanceCtrl,
+      resolve: {
+        span: function(){
+          return span;
+      }
+    }
+    });
+
+    modalInstance.result.then(function() {
+   });
+ };
+
+  var ModalInstanceCtrl = function($scope, $modalInstance, span) {
+
+    $scope.annos = span.annotations;
+    $scope.binaAnnos = span.binaryAnnotations;
+
+
+    $scope.ok = function() {
+      $modalInstance.close($scope);
+    };
+    $scope.cancel = function() {
+      $modalInstance.dismiss('cancel');
+    };
+  };
+  ModalInstanceCtrl.$inject = ['$scope', '$modalInstance', 'span'];
 
 
 
